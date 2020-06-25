dbef0542079f71a52a5eb047bff9b461e9312894
Marc Burnie <MarcABurnie@gmail.com>
2020-06-25 15:50:08 -0500

    maze game working prototype

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 /** @format */
 
-const { Engine, Render, Runner, World, Bodies } = Matter;
+// ===========================================================================
+//                  INITIALIZE CANVAS AND ENGINE
+//============================================================================
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies,
+  Body,
+  Events,
+  MouseConstraint,
+  Mouse,
+} = Matter;
 
 //canvas size
-const width = 600;
-const height = 600;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 //maze size
-const gridRows = 5;
-const gridColumns = 5;
+const gridRows = 10;
+const gridColumns = 10;
 
 const uH = height / gridRows;
 const uW = width / gridColumns;
@@ -16,6 +29,7 @@ const uW = width / gridColumns;
 const lineThickness = 10;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
   element: document.body,
@@ -29,17 +43,27 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-//Walls
-const walls = [
-  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
-];
+// Mouse click and drag functionality
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
 
+// ===========================================================================
+//                  MAZE GENERATION
+//============================================================================
+//Walls/boundaries
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 2, height, { isStatic: true }),
+];
 World.add(world, walls);
 
-//randomize maze order
+//randomize maze order function
 const shuffle = (arr) => {
   let counter = arr.length;
 
@@ -64,9 +88,11 @@ const horizontals = Array(gridRows - 1)
   .fill(null)
   .map(() => Array(gridColumns).fill(false));
 
+//pick random starting location
 const startRow = Math.floor(Math.random() * gridRows);
 const startColumn = Math.floor(Math.random() * gridColumns);
 
+//maze path generation
 const traverse = (row, column) => {
   console.log(row, column);
   //check if visited the cell then return
@@ -117,10 +143,8 @@ const traverse = (row, column) => {
 };
 
 traverse(startRow, startColumn);
-console.log(horizontals);
-console.log(verticals);
-console.log(grid);
 
+//draw horizontal boundaries
 horizontals.forEach((row, r) => {
   row.forEach((open, c) => {
     if (open) return;
@@ -131,24 +155,104 @@ horizontals.forEach((row, r) => {
       lineThickness,
       {
         isStatic: true,
+        label: "wall",
+        render: {
+          fillStyle: "red",
+        },
       }
     );
     World.add(world, wall);
   });
 });
 
+//draw vertical boundaries
 verticals.forEach((row, r) => {
   row.forEach((open, c) => {
     if (open) return;
     const wall = Bodies.rectangle(
-      uH * (c + 1),
+      uW * (c + 1),
       r * uH + uH / 2,
       lineThickness,
       uH + lineThickness,
       {
         isStatic: true,
+        label: "wall",
+        render: {
+          fillStyle: "red",
+        },
       }
     );
     World.add(world, wall);
   });
 });
+
+// ===========================================================================
+//                  RETICULATING SPLINES
+//============================================================================
+const goal = Bodies.rectangle(
+  width - uW / 2,
+  height - uH / 2,
+  uW * 0.7,
+  uH * 0.7,
+  {
+    isStatic: true,
+    label: "goal",
+    render: {
+      fillStyle: "green",
+    },
+  }
+);
+
+World.add(world, goal);
+
+const ball = Bodies.circle(uW / 2, uH / 2, uH > uW ? uW / 4 : uH / 4, {
+  label: "ball",
+  render: {
+    fillStyle: "white",
+  },
+});
+
+World.add(world, ball);
+
+// ===========================================================================
+//                  PLAYER CONTROLS
+//============================================================================
+document.addEventListener("keydown", (e) => {
+  const { x, y } = ball.velocity;
+  if (e.keyCode === 87 || e.keyCode === 38) {
+    Body.setVelocity(ball, { x, y: y - 5 });
+  }
+  if (e.keyCode === 68 || e.keyCode === 39) {
+    Body.setVelocity(ball, { x: x + 5, y });
+  }
+  if (e.keyCode === 65 || e.keyCode === 37) {
+    Body.setVelocity(ball, { x: x - 5, y });
+  }
+  if (e.keyCode === 83 || e.keyCode === 40) {
+    Body.setVelocity(ball, { x, y: y + 5 });
+  }
+});
+
+// ===========================================================================
+//                  WIN CONDITIONS
+//============================================================================
+Events.on(engine, "collisionStart", (e) => {
+  e.pairs.forEach((collision) => {
+    const labels = ["ball", "goal"];
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      win();
+    }
+  });
+});
+
+const win = function () {
+  console.log("user won!");
+  document.querySelector(".winner").classList.remove("hidden");
+  world.gravity.y = 1;
+  world.bodies.forEach((body) => {
+    if (body.label === "wall") Body.setStatic(body, false);
+  });
+};
